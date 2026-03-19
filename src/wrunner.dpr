6372f7731b270045ad59253ee3d526e98b055242
Alessandro <alessandro@gmgsistemi.it>
2026-03-19 09:17:02 +0100

    inizio progetto

---
+++ src/wrunner.dpr
@@ -0,0 +1,14 @@
+program wrunner;
+
+uses
+  Vcl.Forms,
+  MainForm in 'forms\MainForm.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
